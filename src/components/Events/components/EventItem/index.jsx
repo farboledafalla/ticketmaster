190fbe7c6fb159e5c0003165524a79c1b3a115a6
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2023-11-23 19:52:14 -0500

    Se agrega stopPropagation del Componente EventItem

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -5,7 +5,15 @@ const EventItem = ({ info, name, image, onEventClick, id }) => {
          <img src={image} alt={name} width={200} height={200} />
          <h4>{name}</h4>
          <p>{info}</p>
-         <button onClick={() => onEventClick(id)}>Ver más</button>
+         <button
+            onClick={(evt) => {
+               // Parar la propagación del uvento por si el padre llega a tener un evento, este no se ejecute el presionar el padre
+               evt.stopPropagation();
+               onEventClick(id);
+            }}
+         >
+            Ver más
+         </button>
       </div>
    );
 };
