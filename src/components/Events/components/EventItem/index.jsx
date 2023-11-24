f31cad6b31b12f52f0d97687e00e971cde381ee1
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2023-11-23 19:55:17 -0500

    Se agrega el stopPropagation al Componente EventItem desde una funcion

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,19 +1,18 @@
 // Item de los eventos que recibe props
 const EventItem = ({ info, name, image, onEventClick, id }) => {
+   // Función para manejar el evento del botón
+   const handleSeeMoreClick = (evt) => {
+      // Parar la propagación del uvento por si el padre llega a tener un evento, este no se ejecute el presionar el padre
+      evt.stopPropagation();
+      onEventClick(id);
+   };
+
    return (
       <div>
          <img src={image} alt={name} width={200} height={200} />
          <h4>{name}</h4>
          <p>{info}</p>
-         <button
-            onClick={(evt) => {
-               // Parar la propagación del uvento por si el padre llega a tener un evento, este no se ejecute el presionar el padre
-               evt.stopPropagation();
-               onEventClick(id);
-            }}
-         >
-            Ver más
-         </button>
+         <button onClick={handleSeeMoreClick}>Ver más</button>
       </div>
    );
 };
